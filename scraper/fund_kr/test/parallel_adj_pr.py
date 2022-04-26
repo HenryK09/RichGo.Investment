9df00e1c82d76706b4e8a0530662ff492e9fed9b
bohyun_kang <qhgus097@gmail.com>
2022-04-26 11:18:04 +0900

    family_ticker가 빈 값인 예외사항 따로 처리하는 코드가 추가되었습니다. 일자별로 수정가격 업데이트하는 코드 입니다.

---
+++ scraper/fund_kr/test/parallel_adj_pr.py
@@ -0,0 +1,161 @@
+import pandas as pd
+from scraper.fund_kr.api.fund_daily_by_date import get_fund_daily
+import requests
+from scraper.common.dbconn import DBConn
+import os
+
+FUND_DB = os.getenv('FUND_DB_URI')
+
+proxies = {
+    'http': 'socks5://127.0.0.1:9050',
+    'https': 'socks5://127.0.0.1:9050'
+}
+
+
+def get_dividend_added_price(last_trading_dt=None, yesterday=None):
+    url = 'https://dis.kofia.or.kr/proframeWeb/XMLSERVICES/'
+    xml = f"""<?xml version="1.0" encoding="utf-8"?>
+    <message>
+      <proframeHeader>
+        <pfmAppName>FS-DIS2</pfmAppName>
+        <pfmSvcName>DISFundRdmpSO</pfmSvcName>
+        <pfmFnName>select</pfmFnName>
+      </proframeHeader>
+      <systemHeader></systemHeader>
+        <DISCondFuncDTO>
+        <tmpV30>20220408</tmpV30>
+        <tmpV31>20220410</tmpV31>
+        <tmpV12></tmpV12>
+        <tmpV3></tmpV3>
+        <tmpV5></tmpV5>
+        <tmpV4></tmpV4>
+        <tmpV7></tmpV7>
+    </DISCondFuncDTO>
+    </message>
+    """
+    headers = {
+        'Host': 'dis.kofia.or.kr',
+        'Origin': 'https://dis.kofia.or.kr',
+        'Referer': 'https://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/fundann/DISFundRdmp.xml&divisionId=MDIS01004004000000&serviceId=SDIS01004004000'
+    }
+
+    res = requests.post(url, data=xml, headers=headers,
+                        proxies=proxies
+                        ).text
+
+    df = pd.read_xml(res, xpath='.//selectMeta')
+
+    df = df[['tmpV1', 'tmpV2', 'tmpV3', 'tmpV4', 'tmpV5', 'tmpV6', 'tmpV7', 'tmpV8', 'tmpV9', 'tmpV10', 'tmpV11']]
+    df = df.rename(columns={
+        'tmpV1': 'company',
+        'tmpV2': 'product_name',
+        'tmpV3': 'accounting_start',
+        'tmpV4': 'accounting_end',
+        'tmpV5': 'elapsed_days',
+        'tmpV6': 'aum',
+        'tmpV7': 'std_pr',
+        'tmpV8': 'tax_std_pr',
+        'tmpV9': 'distribution_rate',
+        'tmpV10': 'division',
+        'tmpV11': 'ticker'
+    })
+    df['accounting_start'] = pd.to_datetime(df['accounting_start'].astype(str))
+    df['accounting_end'] = pd.to_datetime(df['accounting_end'].astype(str))
+    df['std_pr'] = df['std_pr'].astype(float)
+    df['tax_std_pr'] = df['tax_std_pr'].astype(float)
+    df['aum'] = df['aum'].astype(int)
+    df['distribution_rate'] = df['distribution_rate'].astype(float)
+
+    dvd_df = df[['ticker', 'std_pr', 'accounting_end']].set_index('ticker')
+
+    return dvd_df
+
+
+def get_daily_price_kofia(last_trading_dt=None, today=None):
+    # last_trading_dt =
+    today = pd.Timestamp.now(tz='Asia/Seoul').strftime('%Y%m%d')
+
+    # for verification
+    # kofia_pr_ytdy2 = get_fund_daily('20220407')
+    # kofia_pr_ytdy2 = pd.read_csv('/Users/user/dataknows/20220407.csv', encoding='utf-8-sig')
+    # kofia_pr_ytdy2 = kofia_pr_ytdy2[['ticker', 'base_dt', 'nav']]
+    # kofia_pr_ytdy2['base_dt'] = pd.to_datetime(kofia_pr_ytdy2['base_dt'].astype(str))
+
+    # ytdy_kofia_pr = get_fund_daily('20220408')
+    # tdy_kofia_pr = get_fund_daily('20220411')
+
+    # last_kofia_pr = get_fund_daily(last_trading_dt)
+    # tdy_kofia_pr = get_fund_daily(today)
+
+    last_kofia_pr = pd.read_csv('/Users/user/dataknows/20220408.csv', encoding='utf-8-sig')
+    tdy_kofia_pr = pd.read_csv('/Users/user/dataknows/20220411.csv', encoding='utf-8-sig')
+
+    last_kofia_pr = last_kofia_pr[['ticker', 'base_dt', 'nav']]
+    tdy_kofia_pr = tdy_kofia_pr[['ticker', 'base_dt', 'nav']]
+
+    last_kofia_pr['base_dt'] = pd.to_datetime(last_kofia_pr['base_dt'].astype(str))
+    tdy_kofia_pr['base_dt'] = pd.to_datetime(tdy_kofia_pr['base_dt'].astype(str))
+
+    return last_kofia_pr, tdy_kofia_pr
+
+
+def replace_price(dvd_df):
+    last_kofia_pr, tdy_kofia_pr = get_daily_price_kofia()
+    tdy_kofia_pr = tdy_kofia_pr.set_index('ticker')
+    dvd_df = dvd_df.loc[dvd_df.index.isin(tdy_kofia_pr.index)].copy()
+    tdy_kofia_pr.loc[dvd_df.index, 'adj_nav'] = dvd_df['std_pr']
+    tdy_kofia_pr['adj_nav'] = tdy_kofia_pr['adj_nav'].fillna(tdy_kofia_pr['nav'])
+    kofia_price_chg = pd.merge(last_kofia_pr, tdy_kofia_pr, left_on='ticker', right_on='ticker')
+    kofia_price_chg['price_chg'] = kofia_price_chg['adj_nav'] / kofia_price_chg['nav_x']
+
+    return kofia_price_chg
+
+
+def get_daily_price_db(last_trading_dt=None):
+    # last_trading_dt =
+    query = '''
+        select ticker, base_dt, adj_pr
+        from fund_kofia.product_daily
+        where base_dt = '20220408'
+    '''
+    return DBConn(FUND_DB).fetch(query).df()
+
+
+def main():
+    dvd_df = get_dividend_added_price()
+    kofia_pr_chg = replace_price(dvd_df)
+    db_pr = get_daily_price_db()
+
+    kofia_pr_chg = kofia_pr_chg[['ticker', 'price_chg', 'base_dt_y']]
+    db_pr = db_pr.loc[pd.to_datetime(db_pr['base_dt']) == '2022-04-08']
+    # db_pr = db_pr.loc[pd.to_datetime(db_pr['base_dt'])]
+    db_pr['base_dt'] = pd.to_datetime(db_pr['base_dt'].astype(str))
+    db_pr['adj_pr'] = db_pr['adj_pr'].astype(float)
+
+    df = pd.merge(kofia_pr_chg, db_pr, left_on='ticker', right_on='ticker')
+    df['new_adj_pr'] = df['adj_pr'] * df['price_chg']
+    df = df[['ticker', 'new_adj_pr', 'base_dt_y']]
+    df = df.rename(columns={'base_dt_y': 'base_dt',
+                            'new_adj_pr': 'adj_pr'
+                            })
+
+    return df
+
+
+def update_daily_price():
+    query = '''
+        INSERT INTO fund_kofia.product_daily_practice (ticker, base_dt, adj_pr)
+        VALUES (:ticker, :base_dt, :adj_pr)
+        ON DUPLICATE KEY UPDATE ticker = VALUES(ticker),
+                                base_dt = VALUES(base_dt),
+                                adj_pr = VALUES(adj_pr),
+                                updated_at = now();
+    '''
+    df = main()
+    update_price_list = df.dict(orient='records')
+    with DBConn(FUND_DB).transaction():
+        DBConn(FUND_DB).update(query, update_price_list)
+
+if __name__ == '__main__':
+    update_daily_price()
+    kd = 234
--- scraper/local_test/run_family_fund.py
+++ scraper/local_test/run_family_fund.py
@@ -2,15 +2,21 @@ import pandas as pd
 import requests
 from scraper.common.dbconn import DBConn
 import os
+import time
 
 FUND_DB = os.getenv('FUND_DB_URI')
 CACHE_PATH = os.getenv('CACHE_PATH', '../..')
+
+proxies = {
+    'http': 'socks5://127.0.0.1:9050',
+    'https': 'socks5://127.0.0.1:9050'
+}
 
 
 def get_family_fund(start_dt=None, end_dt=None):
     """
     상위 운용펀드의 코드로 하위 클래스 펀드들을 매핑한다.
-    :param start_dt: 오늘 날짜로부터 6개월 전 날짜
+    :param start_dt: 오늘 날짜로부터 12개월 전 날짜
     :param end_dt: 오늘 날짜
     :return: 공통의 펀드코드로 매핑된 데이터프레임
     """
@@ -23,7 +29,7 @@ def get_family_fund(start_dt=None, end_dt=None):
         else:
             end_dt = pd.Timestamp(end_dt).strftime('%Y%m%d')
         if start_dt is None:
-            start_dt = end_dt - pd.DateOffset(months=6)
+            start_dt = end_dt - pd.DateOffset(months=12)
         else:
             start_dt = pd.Timestamp(start_dt).strftime('%Y%m%d')
 
@@ -31,8 +37,13 @@ def get_family_fund(start_dt=None, end_dt=None):
         start_dt = start_dt.strftime('%Y%m%d')
 
         url = 'https://dis.kofia.or.kr/proframeWeb/XMLSERVICES/'
+        headers = {
+            'Host': 'dis.kofia.or.kr',
+            'Origin': 'https://dis.kofia.or.',
+            'Referer': 'https://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/fundann/DISFundAnnSrch.xml&divisionId=MDIS01001000000000&serviceId=SDIS01001000000'
+        }
 
-        xml_str = f"""<?xml version="1.0" encoding="utf-8"?>
+        xml_main = f"""<?xml version="1.0" encoding="utf-8"?>
         <message>
           <proframeHeader>
             <pfmAppName>FS-DIS2</pfmAppName>
@@ -46,35 +57,35 @@ def get_family_fund(start_dt=None, end_dt=None):
             <vEndDt>{end_dt}</vEndDt>
             <uCdList></uCdList>
             <gbOption>N</gbOption>
-            <uRptList>'2RF12'</uRptList>
-            <tsCd>'2RF12'</tsCd>
+            <uRptList>'2RF05','2RF06', '2RF11','2RF12', '2OF04'</uRptList>
+            <tsCd>'2RF05','2RF06', '2RF11','2RF12', '2OF04'</tsCd>
             <uRptAllYN>0</uRptAllYN>
             <companyCd></companyCd>
         </DISFTimeAnnInsDTO>
         </message>""".encode('utf-8-sig')
 
-        headers = {
-            'Host': 'dis.kofia.or.kr',
-            'Origin': 'https://dis.kofia.or.',
-            'Referer': 'https://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/fundann/DISFundAnnSrch.xml&divisionId=MDIS01001000000000&serviceId=SDIS01001000000'
-        }
-
-        res = requests.post(url, data=xml_str, headers=headers).text
+        res = requests.post(url, data=xml_main, headers=headers).text
         fund_family_df = pd.read_xml(res, xpath='.//list')
+        # fund_family_df.to_csv(cache_file_path, encoding='utf-8-sig')]
+    fund_family_df = fund_family_df[[
+        'standardDt',
+        'uFundNm',
+        'standardCd',
+        'announceTtl'
+    ]]
+    fund_family_df = fund_family_df.rename(
+        columns={
+            'standardDt': 'base_dt',
+            'uFundNm': 'product_name',
+            'standardCd': 'ticker',
+            'announceTtl': 'report_name'
+        }
+    )
 
-        fund_family_df = fund_family_df[['standardDt', 'uFundNm', 'standardCd']]
-        fund_family_df = fund_family_df.rename(
-            columns={
-                'standardDt': 'base_dt',
-                'uFundNm': 'product_name',
-                'standardCd': 'ticker'
-            }
-        )
-        try:
-            fund_family_df = fund_family_df[['product_name', 'ticker']].drop_duplicates(keep='last')
-        except:
-            pass
+    return fund_family_df
 
+
+def processing(fund_family_df):
     index_list = list(range(len(fund_family_df)))
     fund_family_df['is_child'] = fund_family_df['product_name'].str.contains('└▶')
     current_group_id = 0
@@ -99,8 +110,81 @@ def get_family_fund(start_dt=None, end_dt=None):
     family_fund_df = pd.concat(family_fund)
 
     family_fund_df = family_fund_df[['ticker', 'family_ticker']].copy()
+    family_fund_df = family_fund_df.drop_duplicates(['ticker', 'family_ticker'], keep='last')
 
     return family_fund_df
+
+
+def handling_exceptions(start_dt=None, end_dt=None):
+    if end_dt is None:
+        end_dt = pd.Timestamp.now(tz='Asia/Seoul')
+    else:
+        end_dt = pd.Timestamp(end_dt).strftime('%Y%m%d')
+    if start_dt is None:
+        start_dt = end_dt - pd.DateOffset(months=12)
+    else:
+        start_dt = pd.Timestamp(start_dt).strftime('%Y%m%d')
+    end_dt = end_dt.strftime('%Y%m%d')
+    start_dt = start_dt.strftime('%Y%m%d')
+
+    # family_ticker 비어있는 펀드들
+    query = '''
+        select ticker, product_name, listing_dt, trait_division, private_public, family_ticker
+        from fund_kofia.product_info_practice
+        where family_ticker is null and private_public = '공모' and trait_division like '%FUND%'
+        and product_name not like '%지수연계%' and product_name not like '%ELS%' and product_name not like '%목표전환%'
+        order by listing_dt
+    '''
+    ex_df = DBConn(FUND_DB).fetch(query).df()
+    exceptions_list = ex_df['product_name'].tolist()
+
+    url = 'https://dis.kofia.or.kr/proframeWeb/XMLSERVICES/'
+    headers = {
+        'Host': 'dis.kofia.or.kr',
+        'Origin': 'https://dis.kofia.or.',
+        'Referer': 'https://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/fundann/DISFundAnnSrch.xml&divisionId=MDIS01001000000000&serviceId=SDIS01001000000'
+    }
+    ex_list = []
+    for product_name in exceptions_list:
+        xml_ex = f"""<?xml version="1.0" encoding="utf-8"?>
+                <message>
+                  <proframeHeader>
+                    <pfmAppName>FS-DIS2</pfmAppName>
+                    <pfmSvcName>DISFundFTimeAnnSO</pfmSvcName>
+                    <pfmFnName>selectAnn</pfmFnName>
+                  </proframeHeader>
+                  <systemHeader></systemHeader>
+                    <DISFTimeAnnInsDTO>
+                    <uGb>1</uGb>
+                    <vStrtDt>{start_dt}</vStrtDt>
+                    <vEndDt>{end_dt}</vEndDt>
+                    <uCdList>{product_name}</uCdList>
+                    <gbOption>N</gbOption>
+                    <uRptList>'2RF05','2RF06', '2RF11','2RF12', '2OF04'</uRptList>
+                    <tsCd>'2RF05','2RF06', '2RF11','2RF12', '2OF04'</tsCd>
+                    <uRptAllYN>0</uRptAllYN>
+                    <companyCd></companyCd>
+                </DISFTimeAnnInsDTO>
+                </message>""".encode('utf-8-sig')
+
+        res = requests.post(url, data=xml_ex, headers=headers,
+                            proxies=proxies
+                            ).text
+        time.sleep(1)
+        df = pd.read_xml(res, xpath='.//list')
+        df = df[['standardDt', 'uFundNm', 'standardCd', 'announceTtl']]
+        df = df.rename(
+            columns={
+                'standardDt': 'base_dt',
+                'uFundNm': 'product_name',
+                'standardCd': 'ticker',
+                'announceTtl': 'report_name'
+            }
+        )
+        ex_list.append(df)
+    ex_df = pd.concat(ex_list)
+
+    return ex_df
 
 
 def update_family_fund():
@@ -112,10 +196,27 @@ def update_family_fund():
                                 updated_at    = now();
         '''
     family_fund_df = get_family_fund()
+    family_fund_df = processing(family_fund_df)
     update_data_list = family_fund_df.to_dict(orient='records')
     with DBConn(FUND_DB).transaction():
         DBConn(FUND_DB).update(query, update_data_list)
 
 
+def update_ex_fund():
+    query = '''
+        insert into fund_kofia.product_family (ticker, family_ticker)
+        values (:ticker, :family_ticker)
+        ON DUPLICATE KEY UPDATE ticker        = VALUES(ticker),
+                                family_ticker = VALUES(family_ticker),
+                                updated_at    = now();
+        '''
+    exc_df = handling_exceptions()
+    exc_df = processing(exc_df)
+    update_ex_list = exc_df.to_dict(orient='records')
+    with DBConn(FUND_DB).transaction():
+        DBConn(FUND_DB).update(query, update_ex_list)
+
+
 if __name__ == '__main__':
-    update_family_fund()
+    # update_family_fund()
+    update_ex_fund()
